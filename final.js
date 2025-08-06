3932ba3f17e96e2908b5a97ceca7ac408cb515b9
jahidgit-2025 <privately2023@gmail.com>
2025-08-06 07:55:44 +0600

    assignment

---
+++ final.js
@@ -0,0 +1,42 @@
+/** Problem -01 ( Divide the Asset ) */
+var area = 15;
+//write your code here
+var asset = area / 2;
+console.log(asset);
+
+/** Problem -02 ( Cycle or Laptop ) */
+var money = 10000;
+//write your code here
+if (money >= 25000) {
+  console.log("Laptop");
+} else if (money >= 10000) {
+  console.log("Cycle");
+} else {
+  console.log("Chocolate");
+}
+
+/** Problem -03 ( Medicine Planner ) */
+var lastDay = 11;
+//write your code here
+for (var i = 1; i <= lastDay; i++) {
+  if (i % 3 === 0) {
+    console.log(i + "medicine");
+  } else {
+    console.log(i + "rest");
+  }
+}
+
+/** Problem 04 - (Delete / Store) */
+var fileName= "pdfData.jpg";
+//write your code here
+if (fileName.startsWith("#") || fileName.endsWith(".pdf") || fileName.endsWith(".docx")) {
+  console.log("Store");
+} else {
+  console.log("Delete");
+}
+
+/** Problem 05 - ( PH Email Generator )  */
+var student= { name: "jhankar" , roll: 1014 ,department: "cse" };
+//write your code here
+var email = student.name + student.roll + "." + student.department + "@ph.bd";
+console.log(email);
